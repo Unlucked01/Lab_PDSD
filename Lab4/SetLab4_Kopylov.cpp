bdc83d53b083f3a3dcbac56aab079d9c17bdb88a
Unlucked01 <113236909+Unlucked01@users.noreply.github.com>
2023-03-30 15:01:08 +0300

    Lab4

---
+++ Lab4/SetLab4_Kopylov.cpp
@@ -0,0 +1,122 @@
+﻿#include "SetLab4_Kopylov.h"
+
+// F1 Создать пустое множество.
+SetList::SetList() {
+	Set = list<int>();
+}
+// F2 Проверка на пустое множество.
+bool SetList::isEmpty() {
+	return Set.empty();
+}
+// F3 Проверка на принадлежность элемента множеству.
+bool SetList::isInSet(int val) {
+	list<int>::iterator it = find(Set.begin(), Set.end(), val);
+	if (it != Set.end()) {
+		return true;
+	}
+	return false;
+}
+// F4 Добавление нового элемента в начало множества.
+void SetList::addElement(int val) {
+	if (isInSet(val)) {
+		return;
+	}
+	else {
+		Set.push_front(val);
+	}
+}
+// F5 Создание множества.
+SetList::SetList(int length, int minValue, int maxValue, int delim) {
+	Set = list<int>();
+	if (length > 0 && maxValue > minValue && length < maxValue - minValue) {
+		list<int>::iterator pred_node = Set.begin();
+		while (length > 0) {
+			srand(time(NULL));
+			int value = minValue + rand() % (maxValue - minValue);
+			if (value % delim == 0) {
+				addElement(value);
+			}
+			if (Set.begin() != pred_node) {
+				length--;
+			}
+			pred_node = Set.begin();
+		}
+	}
+}
+// F6 Мощность множества.
+int SetList::setSize() {
+	return Set.size();
+}
+// F7 Вывод элементов множества.
+string SetList::printSet(char separator) {
+	if (!isEmpty()) {
+		string result = "";
+		for (int elem : Set) {
+			result += to_string(elem);
+			result += separator;
+		}
+		int length = result.length();
+		return result.erase(length - 1, length);
+	}
+	return "";
+}
+// F8 Удаление множества.
+SetList::~SetList() {
+	Set.clear();
+}
+//F9 Подмножество А-В.
+bool SetList::isSubset(SetList* subSet) {
+	if (isEmpty()) {
+		return true;
+	}
+	for (int elem : Set) {
+
+		if (!isInSet(elem)) {
+			return false;
+		}
+	}
+	return true;
+}
+//F10 Равенство двух множеств А-В.
+bool SetList::areEqual(SetList* second_set) {
+	return isSubset(second_set) && second_set->isSubset(this);
+}
+//F11 Объединение двух множеств.
+SetList* SetList::unionSet(SetList* second_set) {
+	SetList* result_set = new SetList();
+	for (int elem : Set) {
+		result_set->addElement(elem);
+	}
+	for (int elem : second_set->Set) {
+		result_set->addElement(elem);
+	}
+	return result_set;
+}
+//F12 Пересечение двух множеств.
+SetList* SetList::intersectionSet(SetList* second_set) {
+	SetList* result_set = new SetList();
+	for (int elem : second_set->Set) {
+
+		if (isInSet(elem)) {
+			result_set->addElement(elem);
+		}
+	}
+	return result_set;
+}
+//F13 Разность множеств.
+SetList* SetList::differenceSet(SetList* second_set) {
+
+	SetList* result_set = new SetList();
+	for (int elem : Set) {
+		if (!second_set->isInSet(elem)) {
+			result_set->addElement(elem);
+		}
+	}
+	return result_set;
+}
+//F14 Симметричная разность.
+SetList* SetList::symmetricDifferenceSet(SetList* second_set) {
+	SetList* setUnion = unionSet(second_set);
+	SetList* setIntersection = intersectionSet(second_set);
+	return setUnion->differenceSet(setIntersection);
+}
---
+++ Lab4/Set_Cont_List_Kopylov.cpp
@@ -0,0 +1,33 @@
+﻿#include "SetLab4_Kopylov.h"
+
+void delim();
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	SetList* setA = new SetList((rand() % 4) + 6, 10, 99, 3);
+	SetList* setB = new SetList((rand() % 4) + 6, 10, 99, 6);
+	delim();
+	cout << "Вывод множества A, элементы которого кратны 3 - " << setA->printSet(' ') << " (мощность множества - " << setA->setSize() << ")\n";
+	cout << "Вывод множества B, элементы которого кратны 6 - " << setB->printSet(' ') << " (мощность множества - " << setB->setSize() << ")\n";
+	delim();
+    cout << "A является подмножеством B: " << boolalpha << setA->isSubset(setB) << "\n";
+    cout << "A является подмножеством A: " << boolalpha << setA->isSubset(setA) << "\n";
+    delim();
+    cout << "A равняется B: " << boolalpha << setA->areEqual(setB) << "\n";
+    cout << "A равняется A: " << boolalpha << setA->areEqual(setA) << "\n";
+    delim();
+    cout << "Обьединение множеств A и B - " << setA->unionSet(setB)->printSet(' ') << "\n";
+    delim();
+    cout << "Пересечение множеств A и B - " << setA->intersectionSet(setB)->printSet(' ') << "\n";
+    delim();
+    cout << "Разность множеств A и B - " << setA->differenceSet(setB)->printSet(' ') << "\n";
+    cout << "Разность множеств B и A - " << setB->differenceSet(setA)->printSet(' ') << "\n";
+    delim();
+    cout << "Симметричная разность множеств A и B - " << setA->symmetricDifferenceSet(setB)->printSet(' ') << "\n";
+    delim();
+}
+
+void delim() {
+	cout << string(100, '-') << endl;
+}
